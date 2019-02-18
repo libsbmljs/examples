fbb51d9714e1b8d14512f61846d19814e83b397a
0u812 <0u812@users.noreply.github.com>
2019-02-18 15:39:14 -0800

    add rxn glyphs to layout example

--- stable/layout.js
+++ stable/layout.js
@@ -1,13 +1,15 @@
 import libsbml from 'libsbmljs_stable'
 import { rangle } from 'lodash'
-import { readFileSync } from 'fs'
+import { readFileSync, existsSync } from 'fs'
+
+if (!existsSync('models/layout-glycolysis.xml')) {
+  throw new Error('Could not find models/layout-glycolysis.xml; Please ensure sure you running this script from the root directory')
+}
 
 libsbml().then((libsbml) => {
   try {
     const reader = new libsbml.SBMLReader()
-    const sbml = readFileSync('models/layout-glycolysis.xml', 'utf8')
-    console.log(sbml) // sbml
-    const doc = reader.readSBMLFromString(sbml)
+    const doc = reader.readSBMLFromString(readFileSync('models/layout-glycolysis.xml', 'utf8'))
 
     const plugin = doc.getModel().findPlugin('layout')
     // const layoutplugin = plugin.asLayout()
@@ -23,6 +25,32 @@ libsbml().then((libsbml) => {
       console.log('  position:', s.getBoundingBox().getPosition().x(), ', ', s.getBoundingBox().getPosition().y())
       console.log('  width:', s.getBoundingBox().width)
       console.log('  height:', s.getBoundingBox().height)
+    }
+
+    for (const r of layout.rxnglyphs) {
+      console.log('Reaction glyph:', r.getId())
+      console.log('  number of species refs:', r.specref.length)
+      for (const s of r.specref) {
+        console.log('  is curve set?', s.isSetCurve())
+        for (const segment of s.getCurve().segments) {
+          console.log('  segment')
+          console.log('    is cubic Bezier?', segment.isCubicBezier())
+          if (segment.isCubicBezier()) {
+            console.log('    start:', segment.getStart().x(), segment.getStart().y())
+            console.log('    start:', segment.getEnd().x(), segment.getEnd().y())
+            console.log('    start:', segment.getBasePoint1().x(), segment.getBasePoint1().y())
+            console.log('    start:', segment.getBasePoint2().x(), segment.getBasePoint2().y())
+            // expect(segment.getStart().x()).toEqual(225)
+            // expect(segment.getStart().y()).toEqual(85.4117647058823)
+            // expect(segment.getEnd().x()).toEqual(273)
+            // expect(segment.getEnd().y()).toEqual(94.2)
+            // expect(segment.getBasePoint1().x()).toEqual(246)
+            // expect(segment.getBasePoint1().y()).toEqual(91)
+            // expect(segment.getBasePoint2().x()).toEqual(247)
+            // expect(segment.getBasePoint2().y()).toEqual(92)
+          }
+        }
+      }
     }
 
     libsbml.destroy(doc)
